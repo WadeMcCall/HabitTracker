1add2f68863c36adf24ed1217c1b99e115f30eab
WadeMcCall <danceifuwantto@gmail.com>
2023-04-28 11:46:34 -0500

    Added ChatGPT suggested example component

---
+++ habit-tracker/src/Components/HabitList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HabitList = () => {
+  return (
+    <div>
+      <h2>Habits</h2>
+      <ul>
+        <li>Drink water</li>
+        <li>Exercise</li>
+        <li>Read</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HabitList;
